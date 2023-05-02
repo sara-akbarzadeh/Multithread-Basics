729083c0ee763d0d6e4af915f4db9dcc0131666e
Sara <saraakbarzadeh2004@gmail.com>
2023-05-02 23:17:17 +0430

    This is my sixth assignment.

--- src/main/java/sbu/cs/FindMultiples.java
+++ src/main/java/sbu/cs/FindMultiples.java
@@ -34,10 +34,8 @@ public class FindMultiples {
 
         // Create tasks for each multiple of 3, 5, or 7 in the range [1, n]
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                Runnable task = new Task(i);
-                executor.submit(task);
-            }
+            Runnable task = new Task(i);
+            executor.submit(task);
         }
 
         // Wait for all tasks to finish
